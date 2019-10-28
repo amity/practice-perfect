9fe752b5b819030fd3b254de8ee4f3f053a0e6fe
Anna Matusewicz <anna.d.mat@gmail.com>
2019-10-27 22:25:21 -0400

    now

--- practiceperfect/App.js
+++ practiceperfect/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-    View, Button, Text,
+  View, Button, Text,
 } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
@@ -8,60 +8,53 @@ import { Header } from 'react-native-elements';
 import ProfileScreen from './ProfileScreen';
 import MenuScreen from './MenuScreen';
 
+
 class HomeScreen extends React.Component {
     static navigationOptions = ({ navigation }) => {
-        return {
-            headerTitle: 'PracticePerfect',
-            headerRight: (
-                <Button
-                    title="Profile"
-                    onPress={() => navigation.navigate('Profile', { name: 'Jane' })}
-                />
-            ),
-        };
+      return {
+        headerTitle: 'PracticePerfect',
+        headerRight: (
+          <Button
+            title="Profile"
+            onPress={() => navigation.navigate('Profile', { name: 'Jane' })}
+          />
+        ),
+      };
     };
 
-    constructor() {
-        super();
-        this.state = {
-            currentIndex: 0,
-            currentProfileIndex: 0,
-            showBio: false,
-        };
-    }
 
     render() {
-        const { navigate } = this.props.navigation;
-        return (
-            <View style={{ flex: 1 }}>
-                <Header
-                    leftComponent={{ icon: 'menu', color: '#fff', onPress: () => navigate('Menu') }}
-                    centerComponent={{ text: 'PracticePerfect', style: { color: '#fff', fontSize: 20 } }}
-                    rightComponent={(
-                        <Button
-                            title="Profile"
-                            color="#fff"
-                            onPress={() => navigate('Profile')}
-                        />
+      const { navigate } = this.props.navigation;
+      return (
+        <View style={{ flex: 1 }}>
+          <Header
+            leftComponent={{ icon: 'menu', color: '#fff', onPress: () => navigate('Menu') }}
+            centerComponent={{ text: 'PracticePerfect', style: { color: '#fff', fontSize: 20 } }}
+            rightComponent={(
+              <Button
+                title="Profile"
+                color="#fff"
+                onPress={() => navigate('Profile')}
+              />
                     )}
-                    backgroundColor="#e75480"
-                />
-                <Text style={{ flex: 1 }}>
+            backgroundColor="#e75480"
+          />
+          <Text style={{ flex: 1 }}>
                     Hello World!
-                </Text>
-                <View style={{ height: 60 }} />
-            </View>
-        );
+          </Text>
+          <View style={{ height: 60 }} />
+        </View>
+      );
     }
 }
 
 const MainNavigator = createStackNavigator({
-    Home: {
-        screen: HomeScreen,
-        navigationOptions: { header: null },
-    },
-    Profile: { screen: ProfileScreen },
-    Menu: { screen: MenuScreen },
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: { header: null },
+  },
+  Profile: { screen: ProfileScreen },
+  Menu: { screen: MenuScreen },
 });
 const App = createAppContainer(MainNavigator);
 export default App;
--- practiceperfect/ProfileScreen.js
+++ practiceperfect/ProfileScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-    ScrollView, View, Image,
+  ScrollView, View, Image,
 } from 'react-native';
 import { Hoshi } from 'react-native-textinput-effects';
 
@@ -9,35 +9,35 @@ const pic8 = require('./assets/propic.jpg');
 
 export default class ProfileScreen extends React.Component {
     static navigationOptions = {
-        title: 'Profile',
+      title: 'Profile',
     };
 
     render() {
-        return (
-            <View
-                style={{
-                    flex: 1, padding: 1, alignItems: 'center',
-                }}
-            >
-                <ScrollView>
-                    <Image
-                        style={{
-                            flex: 1, height: 200, width: 200, resizeMode: 'cover', borderRadius: 20,
-                        }}
-                        source={pic8}
-                    />
-                    <Hoshi
-                        label="Name"
-                        borderColor="#e75480"
-                        borderHeight={3}
-                    />
-                    <Hoshi
-                        label="Instrument"
-                        borderColor="#e75480"
-                        borderHeight={3}
-                    />
-                </ScrollView>
-            </View>
-        );
+      return (
+        <View
+          style={{
+            flex: 1, padding: 1, alignItems: 'center',
+          }}
+        >
+          <ScrollView>
+            <Image
+              style={{
+                flex: 1, height: 200, width: 200, resizeMode: 'cover', borderRadius: 20,
+              }}
+              source={pic8}
+            />
+            <Hoshi
+              label="Name"
+              borderColor="#e75480"
+              borderHeight={3}
+            />
+            <Hoshi
+              label="Instrument"
+              borderColor="#e75480"
+              borderHeight={3}
+            />
+          </ScrollView>
+        </View>
+      );
     }
 }
