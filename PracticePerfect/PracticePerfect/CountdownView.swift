c72ec367050ea07f98ce2eb938ba3f44ef9ac809
Anna Matusewicz <anna.d.mat@gmail.com>
2019-11-16 14:17:36 -0500

    Done

--- PracticePerfect/PracticePerfect/CountdownView.swift
+++ PracticePerfect/PracticePerfect/CountdownView.swift
@@ -7,30 +7,40 @@
 //
 
 import SwiftUI
+import Combine
 
 struct Countdown: View {
-    @Environment(\.presentationMode) var presentationMode
-    var tempo: Int // Beats per minute
-    var beats: Int // Number to count to
     @Binding var showCountdown: Bool
+    private var tempo: Int // Beats per minute
+    private var beats: Int // Number to count to
+    private var timer: Publishers.Autoconnect<Timer.TimerPublisher>
+
     @State var count = 1
-    let timer = Timer.publish(every: 1, on: .current, in: .common).autoconnect()
+
+    init (tempo: Int, beats: Int, showCountdown: Binding<Bool>) {
+        self.tempo = tempo
+        self.beats = beats
+        self._showCountdown = showCountdown
+
+        let interval = 60.0 / Double(tempo)
+        self.timer = Timer.publish(every: interval, on: .current, in: .common).autoconnect()
+    }
     
     var body: some View {
         Text("\(count)")
             .onReceive(timer) {_ in
-                if self.count < self.beats - 1 {
+                if self.count < self.beats {
                     self.count += 1
                 } else {
                     self.showCountdown = false
                 }
             }
             .font(Font.custom("Arial Rounded MT Bold", size: 100))
-        .background(
-            Circle()
-                .fill(Color.white)
-                .shadow(color: .blue, radius: 10)
-                .frame(width: 200, height: 200)
-        )
+            .background(
+                Circle()
+                    .fill(Color.white)
+                    .shadow(color: .gray, radius: 10)
+                    .frame(width: 200, height: 200)
+            )
     }
 }
